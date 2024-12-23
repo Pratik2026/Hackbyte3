693e25c7bf801ea3fd0963a223a3705674c4e782
rohansen856 <rohansen856@gmail.com>
2024-12-23 22:52:11 +0530

    new: added faq section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,46 @@
-import mytheme from "./utils/theme";
+import mytheme from "./utils/theme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ['class'],
+    darkMode: ["class"],
     content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			primary: mytheme.colors.primary,
-  			secondary: mytheme.colors.secondary,
-  			supporting: mytheme.colors.supporting
-  		},
-  		fontFamily: {
-  			'gotham': ['var(--font-gotham)']
-  		},
-  	}
-  },
-  plugins: [import("tailwindcss-animate")],
-};
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: mytheme.colors.primary,
+                secondary: mytheme.colors.secondary,
+                supporting: mytheme.colors.supporting,
+            },
+            fontFamily: {
+                gotham: ["var(--font-gotham)"],
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: {
+                        height: "0",
+                    },
+                    to: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                },
+                "accordion-up": {
+                    from: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                    to: {
+                        height: "0",
+                    },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
+    },
+    plugins: [import("tailwindcss-animate")],
+}
